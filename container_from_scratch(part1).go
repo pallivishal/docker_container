99a3c4933eb4ad9d52dba28059514ac5c5729a4c
pallivishal <pallivishal2013@gmail.com>
2020-04-30 16:54:56 +0530

    this is a part of container tutorial

---
+++ container_from_scratch(part1).go
@@ -0,0 +1,42 @@
+//intializing go file
+
+package main
+
+//docker                run image <cmd> <params>
+//go run main.go run              <cmd> <params>
+
+//import os
+import "os"
+//creating a new function main
+func main() {
+
+	switch os.Args[1] {
+
+	case "run":
+
+		run()
+
+		default:
+
+			panic("bad command")
+
+		}
+
+	}
+
+//creating a new function run
+func run() {
+
+	fmt.Printf("Running %v \n", os.Args[2:])
+}
+
+//creating  a function must
+func must(err error) {
+
+	if err != nil {
+
+		panic(err)
+
+	}
+
+}
